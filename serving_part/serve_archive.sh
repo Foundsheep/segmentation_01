4687a0d9f5a61cc7e35d9c4840dc5920532dcb3f
Foundsheep <moondeokjong@gmail.com>
2024-08-13 14:51:59 +0900

    fix: sh file to add util files in mar file

--- serving_part/serve_archive.sh
+++ serving_part/serve_archive.sh
@@ -2,6 +2,6 @@ torch-model-archiver --model-name spr_seg \
 --version 1.0 \
 --model-file ../training_part/model_loader.py \
 --serialized-file ./model_store/model.ckpt \
---extra-files ./model_store/labelmap.txt,./model_store/label_to_name.json,./model_store/name_to_label.json \
+--extra-files ./model_store/labelmap.txt,./model_store/label_to_name.json,./model_store/name_to_label.json,../utils \
 --export-path ./model_store \
 --handler ts_handler.py
