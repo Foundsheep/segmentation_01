122a1067f585dbdf5d9db7395e688863de315bda
Foundsheep <moondeokjong@gmail.com>
2024-08-13 14:33:53 +0900

    fix: path in sh file

--- serving_part/serve_archive.sh
+++ serving_part/serve_archive.sh
@@ -2,5 +2,5 @@ torch-model-archiver --model-name spr_seg \
 --version 1.0 \
 --model-file ../training_part/model_loader.py \
 --serialized-file ./model_store/model.ckpt \
---extra-files ./model_store/labelmap.txt, label_to_name.json, name_to_label.json \
+--extra-files ./model_store/labelmap.txt, ./model_store/label_to_name.json, ./model_store/name_to_label.json \
 --handler ts_handler.py
