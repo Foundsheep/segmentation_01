f57112f9d90bb0bcef90ea40dc9e4bec8bdbcce1
Foundsheep <moondeokjong@gmail.com>
2024-08-22 17:12:00 +0900

    fix: width and height position

--- serving_part/ts_handler.py
+++ serving_part/ts_handler.py
@@ -83,7 +83,7 @@ class SPRModelHandler(BaseHandler):
             
         # save
         out_img = Image.fromarray(out_3d)
-        out_img = out_img.resize((h, w))
+        out_img = out_img.resize((w, h))
         
         save_folder = Path(f"./inference_result_{timestamp}")
         if not save_folder.exists():
