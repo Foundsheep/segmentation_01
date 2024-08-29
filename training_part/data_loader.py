fix: data augmentation resizing size and minmax normalisation

training_part/data_loader.py
@@ -102,23 +102,23 @@ class SPRDataset(Dataset):
             img = augmented["image"]
             label_mask = augmented["mask"]
             
-        if idx % 10 == 0:
-            Image.fromarray(img.numpy().transpose(1, 2, 0).astype(np.uint8)).save(f"temp_images/img_{str(idx).zfill(5)}.png")
+        # if idx % 10 == 0:
+        #     Image.fromarray(img.numpy().transpose(1, 2, 0).astype(np.uint8)).save(f"temp_images/img_{str(idx).zfill(5)}.png")
             
-            label_restored = np.zeros((label_mask.shape[0], label_mask.shape[1], 3))
-            for label_idx, tmp_txt in enumerate(self.label_txt):
-                divider_1 = txt.find(":")
-                divider_2 = txt.find("::")
+        #     label_restored = np.zeros((label_mask.shape[0], label_mask.shape[1], 3))
+        #     for label_idx, tmp_txt in enumerate(self.label_txt):
+        #         divider_1 = txt.find(":")
+        #         divider_2 = txt.find("::")
 
-                label_name = txt[:divider_1]
-                label_value = txt[divider_1+1:divider_2]
+        #         label_name = txt[:divider_1]
+        #         label_value = txt[divider_1+1:divider_2]
                 
-                rgb_values = list(map(int, label_value.split(",")))
+        #         rgb_values = list(map(int, label_value.split(",")))
 
-                x, y = np.where(label_mask.numpy() == label_idx)
-                label_restored[x, y, :] = rgb_values
+        #         x, y = np.where(label_mask.numpy() == label_idx)
+        #         label_restored[x, y, :] = rgb_values
             
-            Image.fromarray(label_restored.astype(np.uint8)).save(f"temp_images/lab_{str(idx).zfill(5)}.png")
+        #     Image.fromarray(label_restored.astype(np.uint8)).save(f"temp_images/lab_{str(idx).zfill(5)}.png")
         return img, label_mask
     
     def _read_paths(self):
@@ -177,7 +177,9 @@ class SPRDataset(Dataset):
             # print(f"new ratio: [{img_new.shape[0] / img_new.shape[1]}], new_width: [{img_new.shape[1]}]")
 
         new_ratio = img_new.shape[0] / img_new.shape[1]
+        img_new = np.array(Image.fromarray(img_new).resize((Config.TARGET_WIDTH, Config.TARGET_HEIGHT)))
         assert f"{target_ratio :.2f}" == f"{new_ratio :.2f}", f"{target_ratio =}, {new_ratio =}"
+        assert img_new.shape[0] == Config.TARGET_HEIGHT, f"{img_new.shape = }"
         return img_new
         
 

utils/image_utils.py
@@ -42,22 +42,30 @@ def get_transforms(is_train):
         image = 255 - image
         image = image.astype(np.uint8)
         return image
+    
+    def _lambda_minmax_fn(image, **kwards):
+        image = image.astype(np.float32)
+        image = image / 255.0
+        return image
+    
     if is_train:
         transforms = A.Compose([
-            A.Lambda(image=_lambda_contrast_fn),
+            # A.Lambda(image=_lambda_contrast_fn),
             # A.RGBShift(r_shift_limit=(-50, 50), g_shift_limit=(-50, 50), b_shift_limit=(-50, 50)),
             # A.ColorJitter(brightness=(0.8, 1), contrast=(0.8, 1), saturation=(0.5, 1), hue=(-0.5, 0.5)),
             A.RandomResizedCrop(size=(Config.RESIZED_HEIGHT, Config.RESIZED_WIDTH), scale=(0.8, 1.0), ratio=(0.8, 1.0)),
             A.HorizontalFlip(),
             A.GridDistortion(),            
             A.Blur(),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            A.Lambda(image=_lambda_minmax_fn),
+            # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(transpose_mask=True),
         ])
     else:
         transforms = A.Compose([
             A.Resize(height=Config.RESIZED_HEIGHT, width=Config.RESIZED_WIDTH),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            A.Lambda(image=_lambda_minmax_fn),
             ToTensorV2(transpose_mask=True),
         ])
     return transforms 